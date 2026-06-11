744239dde017c911b50618293a55e8bdeeda190a
Tran Hoang Nhat Nam <trannamnhathoang@gmail.com>
2026-06-11 14:48:25 +0700

    feat: toi uu lai cong thuc tinh gpa

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-def tinh_diem_GPA(diem_so):
-<<<<<<< HEAD
-  return round((diem_so /10) * 4, 2)
-=======
+def tinh_diem_gpa(diem_so):
     if diem_so >= 8.5:
         return 4.0
-    elif diem_so >= 7.0:
-        return 3.0
-    else:
-        return 2.0
->>>>>>> main
+    else:        
+        return (diem_so / 10) * 3.5
+
+print("Điểm GPA hệ 4 là:", tinh_diem_gpa(8.5))
