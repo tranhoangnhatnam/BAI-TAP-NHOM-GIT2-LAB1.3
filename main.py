514f650ea7cae0f21f8d4e3d5facee1afdbf3ac4
Tran Hoang Nhat Nam <trannamnhathoang@gmail.com>
2026-06-17 22:30:37 +0700

    resolve conflict

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 def tinh_diem_GPA(diem_so):
-<<<<<<< HEAD
-  return round((diem_so /10) * 4, 2)
-=======
     if diem_so >= 8.5:
         return 4.0
     elif diem_so >= 7.0:
         return 3.0
     else:
         return 2.0
->>>>>>> main
+
