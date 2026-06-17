8eb29335ded41566cd4ccc2bd8abcc528fb172b9
Tran Hoang Nhat Nam <trannamnhathoang@gmail.com>
2026-06-17 22:39:16 +0700

    fix: sua lai dung cong thuc tinh gpa theo he so 4

--- main.py
+++ main.py
@@ -2,10 +2,4 @@ def tinh_diem_GPA(diem_so):
 
     return round((diem_so / 10) * 4, 2)
 
-    if diem_so >= 8.5:
-        return 4.0
-    elif diem_so >= 7.0:
-        return 3.0
-    else:
-        return 2.0
-
+ 
