a7dce646bd6ea108b28fcce04a60d20f5f1d70ff
Tran Hoang Nhat Nam <trannamnhathoang@gmail.com>
2026-06-17 22:48:11 +0700

    fix: pass all tests

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 def tinh_diem_gpa(diem_so):
-
-    return round((diem_so / 10) * 4, 2)
-
-    
+    if diem_so >= 8.5:
+        return 4.0
+    elif diem_so >= 7.0:
+        return 3.0
+    else:
+        return 2.0
